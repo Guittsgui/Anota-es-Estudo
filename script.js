3c27162a8ecf4c8a58b94541ca0dd75d829b13bc
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-01-31 14:42:43 -0300

    DOM 70%

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const element = document.getElementsByTagName("p")
 const e = document.getElementsByClassName("subtitle")
 const t = document.querySelector("#title")
 
+
 function clicou(){
-    if (t.innerHTML == "Guia de Estudos"){
-        t.innerHTML = "Clicou e Mudou"
-    }else {
-        t.innerHTML = "Guia de Estudos"
-    }
+    t.classList.toggle('teste')
+
 }
 
-t.addEventListener("click","clicou()")
+
+
+
+t.addEventListener("click",() => {
+    clicou()
+})
